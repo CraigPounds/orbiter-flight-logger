3185dad0265c1c43591b72bb35c12b548b45eca1
Craig Pounds <nweaver@windstream.net>
2018-09-22 08:37:10 -0500

    Connected to db

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
 'use strict';
 
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/flight-log-db';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-flight-log-db';
+exports.PORT = process.env.PORT || 8080;
