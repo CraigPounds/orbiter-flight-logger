68d58e291b306f335f8e1acc0a11ddcf011ebc5f
Craig Pounds <nweaver@windstream.net>
2018-09-22 01:53:23 -0500

    Added test for hitting root URL

--- test/logger-test.js
+++ test/logger-test.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+
+const { app } = require('../server');
+chai.use(chaiHttp);
+
+describe('hit root URL', function() {
+  it('should return status code 200 and HTML', function() {
+    let res;
+    return chai.request(app)
+      .get('/')
+      .then(function(_res) {
+        res = _res;
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
+      });
+  });
+});
