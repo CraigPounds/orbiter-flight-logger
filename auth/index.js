d0c880e0fc8562cc42e5661b7c0b5b682456a9ab
Craig Pounds <nweaver@windstream.net>
2018-10-13 14:48:37 -0500

    Added SECRET_STRING to Travis Evnironmental variables - duh!

--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,5 @@
 'use strict';
 const {router} = require('./router');
 const {localStrategy, jwtStrategy} = require('./strategies');
-console.log(jwtStrategy);
+
 module.exports = {router, localStrategy, jwtStrategy};
