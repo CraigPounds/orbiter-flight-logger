6cb1d15b6ecc0be8cc5a267b080f098e32e91a0e
Craig Pounds <nweaver@windstream.net>
2018-10-14 07:43:21 -0500

    Re-added useCreateIndex and useNewUrlParser

--- server.js
+++ server.js
@@ -46,9 +46,7 @@ let server;
 
 function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    // mongoose.connect(databaseUrl, { useCreateIndex: true , useNewUrlParser: true }, err => {
-    // mongoose.connect(databaseUrl, { useCreateIndex: true }, err => {
-    mongoose.connect(databaseUrl, err => {
+    mongoose.connect(databaseUrl, { useCreateIndex: true , useNewUrlParser: true }, err => {
       if (err) {
         return reject(err);
       }
