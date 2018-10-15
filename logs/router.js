6acbc7da7f31a73145a57a1aa02e5c8de1e48bac
Craig Pounds <nweaver@windstream.net>
2018-10-15 13:51:02 -0500

    POST endpoint and test for logs

--- logs/router.js
+++ logs/router.js
@@ -7,8 +7,53 @@ const jsonParser = bodyParser.json();
 const passport = require('passport');
 const jwtAuth = passport.authenticate('jwt', { session: false });
 const { Log } = require('./models');
+const { Mission } = require('../missions');
 
-
+router.post('/', (req, res) => {
+  const requiredFields = ['mission_id', 'title'];
+  requiredFields.forEach(field => {
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  });
+  Mission
+    .findById(req.body.mission_id)
+    .then(mission => {
+      if (mission) {
+        Log
+          .create({
+            mission_id: req.body.mission_id,
+            title: req.body.title,
+            vessel: req.body.vessel,
+            date: req.body.date,
+            log: req.body.log
+          })
+          .then(logPost => res.status(201).json({            
+            _id: logPost.id,
+            mission_id: logPost.mission_id,
+            title: logPost.title,
+            vessel: logPost.vessel,
+            date: logPost.date,
+            log: logPost.log
+          }))
+          .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: 'Internal server error'});
+          });
+      }
+      else {
+        const message = 'Mission not in database';
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error'});
+    });
+});
 
 router.get('/', (req, res) => {
   Log
--- test/test-logs.js
+++ test/test-logs.js
@@ -7,6 +7,7 @@ const faker = require('faker');
 const mongoose = require('mongoose');
 const { User } = require('../users');
 const { Mission } = require('../missions');
+const { Log } = require('../logs');
 const { app, runServer, closeServer } = require('../server');
 const { TEST_DATABASE_URL } = require('../config');
 const { seedUserData, seedMissionData, tearDownDb, gernerateUserName, generateUserPassword } = require('./test-flight-logger');
@@ -30,5 +31,41 @@ describe('Logs endpoints', function() {
     return closeServer();
   }); 
 
+  describe('POST logs endpoint', function() {
+    it('should add a log by mission', function() {
+      let newLog;
+      return Mission
+        .findOne()
+        .then(function(mission) {          
+          newLog = {
+            mission_id: mission._id,
+            title: faker.lorem.sentence(),
+            vessel: faker.lorem.word(),
+            date: faker.lorem.word(),
+            log: faker.lorem.paragraph()
+          };
+          return chai.request(app)
+            .post('/logs')
+            .send(newLog)
+            .then(function(res) {
+              expect(res).to.have.status(201);
+              expect(res).to.be.json;
+              expect(res.body).to.be.a('object');
+              expect(res.body).to.include.keys('_id', 'mission_id', 'title', 'vessel', 'date', 'log');
+              expect(res.body._id).to.not.be.null;
+              return Log.findById(res.body._id);
+            })
+            .then(function(log) {
+              let logUserId = log.mission_id.toString();
+              let newLogUserId = newLog.mission_id.toString();
+              expect(logUserId).to.equal(newLogUserId);
+              expect(log.title).to.equal(newLog.title);
+              expect(log.vessel).to.equal(newLog.vessel);
+              expect(log.date).to.equal(newLog.date);
+              expect(log.log).to.equal(newLog.log);
+            });
+        });     
+    });
+  });
     
 });
